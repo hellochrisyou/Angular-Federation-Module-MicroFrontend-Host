73391d32c65b473dd761d0ec04eeb156413561aa
Chris You <hellochrisyou@gmail.com>
2023-08-03 20:14:52 -0400

    Working Copy. Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'test',
+    loadChildren: () => import('testRemote/Module')
+    .then(m => m.TestModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wrapper-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'shell-test';
+
+  constructor(
+    private router: Router
+  ) {
+    console.log('constructor in shell initiated');
+  }
+
+  navigate() {
+    this.router.navigateByUrl('test');
+  }
 }
